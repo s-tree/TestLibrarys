7d5e0d848380dee5b7e53b57b3b990150351ffcd
s_tree <344096470@qq.com>
2019-03-15 08:52:16 +0800

    zhushi

--- app/src/main/java/com/jingxi/smartlife/pad/sdk/demo/dooraccess/DoorAccessSecurityActivity.java
+++ app/src/main/java/com/jingxi/smartlife/pad/sdk/demo/dooraccess/DoorAccessSecurityActivity.java
@@ -11,6 +11,7 @@ import com.intercom.sdk.SecurityMessage;
 import com.intercom.sdk.SmartHomeManager;
 import com.jingxi.smartlife.pad.sdk.JXPadSdk;
 import com.jingxi.smartlife.pad.sdk.demo.R;
+import com.jingxi.smartlife.pad.sdk.doorAccess.DoorAccessManager;
 import com.jingxi.smartlife.pad.sdk.doorAccess.base.DoorSecurityUtil;
 
 import java.util.List;
@@ -24,6 +25,15 @@ public class DoorAccessSecurityActivity extends AppCompatActivity implements Doo
         setContentView(R.layout.activity_dooraccess_list);
         textView = (TextView) findViewById(R.id.textView);
         JXPadSdk.getDoorAccessManager().addSecurityListener(this);
+        DoorAccessManager doorAccessManager = JXPadSdk.getDoorAccessManager();
+        /**
+         * 查询安防状态
+         */
+        doorAccessManager.querySecurityStatus(DoorAccessMainActivity.familyID);
+        /**
+         * 取消报警
+         */
+        doorAccessManager.cancelSecurityWarning(DoorAccessMainActivity.familyID);
     }
 
     public void toPlayBack(View v){
@@ -40,16 +50,33 @@ public class DoorAccessSecurityActivity extends AppCompatActivity implements Doo
         JXPadSdk.getDoorAccessManager().removeSecurityListener(this);
     }
 
+    /**
+     * 安防状态变更
+     * @param familyDockSn
+     * @param state
+     * @param isFromQuery
+     */
     @Override
     public void onStateChanged(String familyDockSn, int state, boolean isFromQuery) {
         textView.setText("安防状态变更： " + familyDockSn + " 状态 ： " + state + " isFromQuery = " + isFromQuery);
     }
 
+    /**
+     * 安防设备报警
+     * @param familyDockSn
+     * @param stateBeans
+     * @param device
+     */
     @Override
     public void onAlarm(String familyDockSn, List<SecurityMessage.StateBean> stateBeans, SmartHomeManager.SecurityDevice device) {
         textView.setText("安防设备报警 ： " + familyDockSn + " 设备 " + stateBeans.get(0).getAlias());
     }
 
+    /**
+     * 安防取消报警回调
+     * @param familyDockSn
+     * @param device
+     */
     @Override
     public void onCancelAlarm(String familyDockSn, SmartHomeManager.SecurityDevice device) {
         textView.setText("防区解除报警 ： " + familyDockSn);
